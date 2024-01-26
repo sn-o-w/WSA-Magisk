ca796d7a0cf84878ba6ead77c8f0361ecd44c3e4
sn-o-w <cristian.silaghi@mozilla.ro>
2024-01-26 22:30:58 +0200

    Misc changes + Magisk Delta + Magisk Alpha + WSA Preview Builds

--- scripts/run.sh
+++ scripts/run.sh
@@ -68,11 +68,11 @@ ARCH=$(
 
 RELEASE_TYPE=$(
     Radiolist '([title]="WSA release type"
-                [default]="retail")' \
-        'retail' "Stable Channel" 'on' \
+                [default]="insider fast")' \
+        'retail' "Stable Channel" 'off' \
         'release preview' "Release Preview Channel" 'off' \
         'insider slow' "Beta Channel" 'off' \
-        'insider fast' "Dev Channel" 'off'
+        'insider fast' "Dev Channel" 'on'
 )
 
 if (YesNoBox '([title]="Root" [text]="Do you want to Root WSA?")'); then
@@ -89,11 +89,13 @@ fi
 if [ "$ROOT_SOL" = "magisk" ]; then
     MAGISK_VER=$(
         Radiolist '([title]="Magisk version"
-                    [default]="stable")' \
-            'stable' "Stable Channel" 'on' \
+                    [default]="delta")' \
+            'stable' "Stable Channel" 'off' \
             'beta' "Beta Channel" 'off' \
             'canary' "Canary Channel" 'off' \
-            'debug' "Canary Channel Debug Build" 'off'
+            'debug' "Canary Channel Debug Build" 'off' \
+            'delta' "Delta Version by HuskyDG" 'on' \
+            'alpha' "Alpha Version by vvb2060" 'off'
     )
 else
     MAGISK_VER=""
@@ -148,9 +150,9 @@ fi
 if [ "$COMPRESS_OUTPUT" = "--compress" ]; then
     COMPRESS_FORMAT=$(
         Radiolist '([title]="Compress format"
-                    [default]="7z")' \
-            '7z' "7-Zip" 'on' \
-            'zip' "Zip" 'off'
+                    [default]="zip")' \
+            '7z' "7-Zip" 'off' \
+            'zip' "Zip" 'on'
     )
 fi
 
